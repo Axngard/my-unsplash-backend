a7919616b8f5d1a646dc58934ae239d598ce15f0
Axel Espinosa <axeldavid45@gmail.com>
2020-10-19 21:54:45 -0500

    Configuration module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from "./config/configuration";
 
 @Module({
-  imports: [],
+  imports: [UserModule, ConfigModule.forRoot({
+    load: [configuration]
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
