43aca85378779b3ebcdaca6ab421cdbbe5881c7a
Axel Espinosa <axeldavid45@gmail.com>
2020-10-20 19:29:22 -0500

    Added a route description swagger

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,14 +1,16 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { SignInDto } from './dtos/signin.dto';
 import { AuthService } from './auth.service';
-import { ApiOkResponse } from '@nestjs/swagger';
+import {ApiOkResponse, ApiOperation, ApiTags} from '@nestjs/swagger';
 import { SignInResponseDto } from './dtos/sign-in-response.dto';
 
 @Controller('auth')
+@ApiTags('Authentication')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
+  @ApiOperation({summary: 'Issue a JWT to a User'})
   @ApiOkResponse({
     description: 'Generates a JWT to perform requests',
     type: SignInResponseDto,
